e921b2defd18aeb36e5e7810ffca61cfb34c6c25
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-10 23:37:17 +0300

    :P

---
+++ SpectatorReplaceSCP/EventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+namespace SpectatorReplaceSCP
+{
+    public class EventHandler
+    {
+        public static void PlayerLeft(LeftEventArgs ev)
+        {
+            if (ev.Player.Role.Side != Exiled.API.Enums.Side.Scp) return;
+            Player player = Player.List.Where(x => x.Role.Type == RoleTypeId.Spectator).GetRandomValue();
+            player.Role.Set(ev.Player.Role);
+            player.Position = ev.Player.Position;
+            player.MaxHealth = ev.Player.MaxHealth;
+            player.Health = ev.Player.Health;
+            player.DisplayNickname = player.Nickname + " | REPLACER";
+            player.HumeShield = ev.Player.HumeShield;
+            player.MaxArtificialHealth = ev.Player.MaxArtificialHealth;
+            player.ArtificialHealth = ev.Player.ArtificialHealth;
+        }
+    }
+}
---
+++ SpectatorReplaceSCP/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace SpectatorReplaceSCP
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "6hundred9";
+        public override string Name => "SpectatorReplaceSCP";
+        public override string Prefix => "srSCP";
+        public override Version RequiredExiledVersion => new Version(8, 8,0);
+        public override bool IgnoreRequiredVersionCheck => true;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Left += EventHandler.PlayerLeft;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Left -= EventHandler.PlayerLeft;
+            base.OnDisabled();
+        }
+    }
+}
